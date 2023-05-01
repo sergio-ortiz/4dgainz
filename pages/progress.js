10782fa8479ac4742fc14158cad70ab7da44933a
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2023-05-01 14:52:32 -0400

    feat(progress): page component

--- pages/progress.js
+++ pages/progress.js
@@ -1,5 +1,19 @@
 import Layout from "@/components/Layout";
+import styles from "../styles/Progress.module.css";
 
-const Progress = () => <Layout>Progress</Layout>;
+const Progress = () => (
+  <Layout>
+    <div className={styles.container}>
+      <h1 className={styles.mainHeading}>Progress</h1>
+      <hr />
+      <div className={styles.flexBox}>
+        <p>Lorem ipsum dlor sit amet, consectetur adipiscing elit. Donec tincidunt ante quis orci interdum, eget aliquam sem condimentum. Aliquam id tincidunt lectus. Fusce dignissim nunc.</p>
+        <p>Donec tincidunt ante quis orci interdum, eget aliquam sem condimentum. Aliquam id tincidunt lectus. Fusce dignissim nunc.</p>
+        <p> Aliquam id tincidunt lectus. Fusce dignissim nunc. Donec tincidunt ante quis orci interdum, eget aliquam sem condimentum.</p>
+        <div className={styles.progressCircle}>75%</div>
+      </div>
+    </div>
+  </Layout>
+);
 
 export default Progress;
