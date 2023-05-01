40653723da20e56fab6bebcebacec63313de11bc
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2023-05-01 12:40:13 -0400

    feat(stats): page component skeleton

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import Layout from "@/components/Layout";
+import styles from "../styles/Home.module.css";
 
-const Home = () => <Layout>AllForTheGainz</Layout>;
+const Home = () => (
+  <Layout>
+    <div className={styles.container}>
+      <h1 className={styles.mainHeading}>AllForTheGainz</h1>
+    </div>
+  </Layout>
+);
 
 export default Home;
--- pages/stats.js
+++ pages/stats.js
@@ -1,5 +1,20 @@
 import Layout from "@/components/Layout";
+import styles from "../styles/Stats.module.css";
 
-const Stats = () => <Layout>Stats</Layout>;
+const Stats = () => (
+  <Layout>
+    <div className={styles.container}>
+      <h1 className={styles.mainHeading}>Stats</h1>
+      <hr />
+      <ul className={styles.stats}>
+        <li className={styles.statsItem}>Height:</li>
+        <li className={styles.statsItem}>Weight:</li>
+        <li className={styles.statsItem}>Sex:</li>
+        <li className={styles.statsItem}>Age:</li>
+        <li className={styles.statsItem}>Activity:</li>
+      </ul>
+    </div>
+  </Layout>
+);
 
 export default Stats;
