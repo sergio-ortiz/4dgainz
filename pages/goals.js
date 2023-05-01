ffe27ae455d77fdb6cf5ba9f81682cc9ecce5841
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2023-05-01 13:25:01 -0400

    feat(goals): page component skeleton based off stats page component

--- pages/goals.js
+++ pages/goals.js
@@ -1,5 +1,18 @@
 import Layout from "@/components/Layout";
+import styles from "../styles/Stats.module.css";
 
-const Goals = () => <Layout>Goals</Layout>;
+const Goals = () => (
+  <Layout>
+    <div className={styles.container}>
+      <h1 className={styles.mainHeading}>Goals</h1>
+      <hr />
+      <ul className={styles.stats}>
+        <li className={styles.statsItem}>Weight:</li>
+        <li className={styles.statsItem}>Measurements:</li>
+        <li className={styles.statsItem}>Plans:</li>
+      </ul>
+    </div>
+  </Layout>
+);
 
 export default Goals;
